cdca69f0269602dfbbec5fd1aa4ab83dddf54bf8
Vural Yilmaz <vyilmaz@stud.hs-bremen.de>
2016-04-25 15:39:34 +0200

    Aufteilung der Controller

---
+++ src/main/java/controller/FilmController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import interfaces.FilmFacade;
+import model.Film;
+import model.Filter;
+
+public class FilmController implements FilmFacade {
+
+	@Override
+	public void filmErstellen(Film film) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void filmSuchen(String suche) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void filmSuchenErweitert(Film film) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void filmBewerten(Long FID, double sterne) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void filmKommentieren(Long FID, String kommentar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void filmFiltern(Filter filter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
