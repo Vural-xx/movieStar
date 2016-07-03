3308302aa6e3f5630b6acac93de7c1796265f1e2
Vural Yilmaz <vyilmaz@stud.hs-bremen.de>
2016-07-03 22:41:27 +0200

    Import.sql jetzt mit benutzer und admin

--- src/main/java/import.sql
+++ src/main/java/import.sql
@@ -1,1 +1,2 @@
-INSERT INTO `mydatabase`.`Benutzer` (`email`, `benutzername`, `benutzerstatus`, `passwort`) VALUES ('test@test.de', 'test', 'FREIGESCHALTET', 'Test#123');
+INSERT INTO `Benutzer` (`email`, `benutzername`, `benutzerstatus`, `passwort`, `rolle`) VALUES ('test@test.de', 'test', 'FREIGESCHALTET', 'Test#123', 'BENUTZER')
+INSERT INTO `Benutzer` (`email`, `benutzername`, `benutzerstatus`, `passwort`, `rolle`) VALUES ('admin@test.de', 'admin', 'FREIGESCHALTET', 'Test#123', 'ADMIN')
